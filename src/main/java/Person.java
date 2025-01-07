038fd8a8d977de5ee7252107b37101dc918a251d
pc9 <pc9@LAB-A--PC9.lan>
2025-01-07 19:54:43 +0200

    Main Class

---
+++ src/main/java/Person.java
@@ -0,0 +1,74 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author pc9
+ */
+public class Person {
+    
+    private String ID;
+
+    /**
+     * Get the value of ID
+     *
+     * @return the value of ID
+     */
+    public String getID() {
+        return ID;
+    }
+
+    /**
+     * Set the value of ID
+     *
+     * @param ID new value of ID
+     */
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    private String Name;
+
+    /**
+     * Get the value of Name
+     *
+     * @return the value of Name
+     */
+    public String getName() {
+        return Name;
+    }
+
+    /**
+     * Set the value of Name
+     *
+     * @param Name new value of Name
+     */
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+
+    private int age;
+
+    /**
+     * Get the value of age
+     *
+     * @return the value of age
+     */
+    public int getAge() {
+        return age;
+    }
+
+    /**
+     * Set the value of age
+     *
+     * @param age new value of age
+     */
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+
+    
+}
